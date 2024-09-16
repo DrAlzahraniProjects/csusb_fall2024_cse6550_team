f2a6eda8299b7663feb2527216660ff3ea69757c
Dr Alzahrani <alzahran@csusb.edu>
2024-09-15 20:44:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-# Your Streamlit app code
-st.title("Hello, Streamlit!")
+def main():
+    st.title('My Streamlit App')
+    st.write('Hello, world!')
 
-# Ensure the app runs on port 5005
 if __name__ == "__main__":
-    st.run("app.py", server_port=5005, server_address="0.0.0.0")
+    main()
